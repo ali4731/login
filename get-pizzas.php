8e4403a3f1d18bcd1acb5d57f65113d7c49b910d
Ali <aliakram4731@gmail.com>
2025-04-25 13:09:42 +0500

    Update get-pizzas.php

--- get-pizzas.php
+++ get-pizzas.php
@@ -12,32 +12,3 @@ foreach ($pizzas as $pizza) {
     echo htmlspecialchars($pizza['name']) . "<br>";
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-
-<p>
-    <p>
-        <p>
-            <p>
-                <p>
-                    <p>
-                        <p>
-                            <p>
-                                
-                            </p>
-                        </p>
-                    </p>
-                </p>
-            </p>
-        </p>
-    </p>
-</p>
-</body>
-</html>
